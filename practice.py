77ea6d948af868e5d804c8191987f21217c10a94
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-06 09:12:36 +0600

    add some code

--- practice.py
+++ practice.py
@@ -26,5 +26,57 @@
 
 # isAlNum = checking that is that a alphanumeric.if it contains alphabet and numeric value return true
 
-alphanumeric = "NasirMIa123";
-print(alphanumeric.isalnum())
+# alphanumeric = "NasirMIa123";
+# print(alphanumeric.isalnum())
+
+
+# isAlpha method= checking that is it contains only alphabet characters
+
+# alpha = "alkfaslkfasdfklj";
+# print(alpha.isalpha())
+
+
+# isnumeric,isdegit,isdecimal method in python
+# print("123".isdigit())
+# print("123".isnumeric())
+# print("123".isdecimal())
+
+
+# isidentifier = check is any string can be used as a variable or not. if so then true otherwise false
+# identifier = "abc-4";
+# print(identifier.isidentifier()) # it will return false because "-" can't use in variable 
+
+# identifier2 = "name";
+# print(identifier2.isidentifier());  #it will return false because we can use name as a  variable.
+
+
+# islower checks is any string is fully lowercase or not
+# name = "nasir";
+# print(name.islower());
+
+
+
+# isupper checks is any string is fully uppercase or not
+# name = "NASIR";
+# print(name.islower());
+
+
+# isprintable checks is any string contains any escaped character like \n
+# printable = "Nasir\n";
+# print(printable.isprintable());
+
+
+# isspace checks is any string contains only spaces or not
+# space = "    ";
+# print(space.isspace())
+
+
+# istitle checks is any setntence contain every word with the uppercase like "My First Video Title".
+# title = "My First Video Title";
+# print(title.istitle());    #it will return true because it contains every word first letter with uppercase.
+
+
+
+# join method is use to join multiple words together with given character
+# name = ["my","name","is","Nasir"];
+# print(" ".join(name));
