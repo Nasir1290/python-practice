1d0c271ec598a9522ad7110cb672404b8803c1f1
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-06 08:31:50 +0600

    add some code

--- practice.py
+++ practice.py
@@ -4,8 +4,27 @@
 # text = "hello world";
 # print(text.count('o'));
 
+# find in python
 # text = "hello world";
 # position = text.find("world");
 # print(text[:position-1])
 
+# format method
+# name = "Nasir";
+# age = 21;
+# print("hello {} , though you are {} years old , so you are eligible".format(name,age));
+# print("hello {} , though you are {} years old, so you are eligible".format(age,name))
 
+
+
+# index method
+
+# name = "Nasir";
+# print(name.index('r'));
+
+
+
+# isAlNum = checking that is that a alphanumeric.if it contains alphabet and numeric value return true
+
+alphanumeric = "NasirMIa123";
+print(alphanumeric.isalnum())
