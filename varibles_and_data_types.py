4a4bc282548da318262c9a01e9e1a5106e8a7d1f
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-10 06:35:14 +0600

    practice variables and data types

--- varibles_and_data_types.py
+++ varibles_and_data_types.py
@@ -1,4 +1,14 @@
-a = "nasir"
-b="b"
-print(a)
+# variable naming conventions:
+# name should only contain letters , numbers and underscores. it cant contain some special characters like @,%,$,! etc.
+# name should start with a letter or underscore but the convation is to start it with a letter.
+
+# name="nasir"
+
+
+# boolean values
+# boolean values can be either True or False.and their first character is uppercase like True or False.
+# a = True
+# b=False
+# print(a)
+# print(b)
 
