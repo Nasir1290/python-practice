33fa5a7eba1a1914a7c13c122cd0d0f934a8d1ba
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-13 18:48:54 +0600

    variables and operations

--- strings_and_conditional_statements.py
+++ strings_and_conditional_statements.py
@@ -1,8 +1,15 @@
 # Strings and conditional statements in python 👇👇
 
+# string writing types.... 
 
-a='this ia a single qoute string'
-b="this a double qoute string"
-c="""this a triple qoute string"""
+# a='this ia a single qoute string'
+# b="this a double qoute string"
+# c="""this a triple qoute string"""
 
+# escape characters in python
 
+# next_line="this is a string with next line escape characters.\nthis a next line string"
+# print(next_line)
+
+tab="this is a string with tab escape characters.\tthis a tab string"
+print(tab)
