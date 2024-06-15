c9d137e20122fed9f3a4058fbfa470116e766ff8
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-15 09:49:54 +0600

    strings operations and conditional rendering

--- strings_and_conditional_statements.py
+++ strings_and_conditional_statements.py
@@ -37,10 +37,67 @@
 # Slicing in python means is acces a part of a string
 # in case of slicing the indexing start from 1.
 
-str = "this is a string"
-# in the argument the first one is starting index and last one is end index.
-sliced = str[1:4]
-print(sliced)
 
-# inclusive
+# # slicing with negative steps 👇👇
+# str = "this is a string"
+# # the last step -2 is the step and it will iterate the str from negative indexing and take every second charecter from the current index
+# print(str[14:5:-2])
 
+
+# # shortly print the whole iterative in negative sequence 👇👇
+# str = "this is a string"
+# print(str[::-1])
+
+
+# # mixing positive and negative indexes👇👇
+# str = "this is a string"
+# print(str[5:-5])
+
+
+# # reverse slicing with a negative skipping index 👇👇
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# print(lst[::-2])
+
+
+# # partial reverse slicing 👇👇
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# print(lst[6:2:-1])
+
+
+# now let's see all string function 👇👇👇
+
+# # endsWith function check weather a string ends with given substring or not and it returns True false 👇👇
+# name="nasir"
+# print(name.endswith("nasi"))
+
+
+# # capitalize function make the first letter of the string capitalize
+# name="nasir"
+# print(name.capitalize())
+
+
+# # replace function replace particular value with the given value
+# name="nasir"
+# name = name.replace("sir","hid")
+# print(name)
+
+
+# # find function return the first occurence index of the given value or charecter
+# name="nasir"
+# print(name.find("a"))
+
+
+# # count function return the number of occurences of the given value. like how many times it is in the code
+# name = "nasir mia is a good programmer"
+# characters = []
+# for char in name:
+#     if char not in characters:
+#         characters.append(char)
+#         # count occurences
+#         print(char,": occures ",name.count(char)," times")
+# print(len(characters))
+
+
+# conditional statements with (if , elif, else) 👇👇
+# confusion is with if and elif
+# elif will only run if the if statement is false and if if statement is true then elif will not run ..
