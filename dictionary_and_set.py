fa89b2b9e0b8c62bcff64137b9c0b130a81be0b9
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-17 19:24:37 +0600

    practice dictionary and set in python

--- dictionary_and_set.py
+++ dictionary_and_set.py
@@ -2,6 +2,27 @@
 
 
 # Dictionary in python 👇
-# we can check the dictionary length
-dic = {"name":"Nasir","roll":568427}
-print(dic)
+
+
+# # len() function => we can check the dictionary length
+# dic = {"name":"Nasir","roll":568427}
+# print(len(dic))
+
+
+# # dict() function => we can create a dictionary with this function
+# name="Nasir"
+# roll=568427
+# dic=dict(name=name,roll=roll)
+# print(dic)
+
+
+# # clear() function => clear all item from the dictionary
+# dic={"name": "Nasir", "roll":568427}
+# dic.clear()
+# print(dic)
+
+
+# copy() function => work like other datatype in dictionary like list 
+dic={"name": "Nasir", "roll":568427}
+dic2=dic.copy()
+print(dic2.__reversed__)
