34424422ec304e47fd86838c9bb2c74bc8a22b0d
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-18 20:44:36 +0600

    practicing loops in python

--- loops.py
+++ loops.py
@@ -1,1 +1,17 @@
 # Loops in python 👇👇👇
+
+# Loops in Python are used to execute a block of code repeatedly. Python supports two types of loops: (1)for loops and (2)while loops
+
+
+# for loops👇👇
+
+# # iterating ovet a list 
+# fruits = ["apple", "orange", "blackberry","mango","banana","cupcake","cherry"]
+# for fruit in fruits:
+#     print(fruits.index(fruit)+1,":",fruit)
+
+
+# iterating overa  range of numbers
+
+for num in range(1,6):
+    print(num)
