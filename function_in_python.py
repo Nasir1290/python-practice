c282253e7f473e2e978c228f4b1c1e3a47bb92b8
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-20 19:35:14 +0600

    Practice function in python

--- function_in_python.py
+++ function_in_python.py
@@ -7,4 +7,31 @@
 # my_func()
 
 
-# 
+# # function with parameters..
+# def num_sum(num1,num2):
+#     sum=num1+num2
+#     return sum
+# get_sum=num_sum(2,3)
+# print(get_sum)
+
+
+# # here's in the previous example we take two parametes , but what if the input parameter are more than two?This time the code will crash.To handle this we can use *args and **kwargs
+# def get_sum(num1,*args):
+#     sum1=sum((num1,)+args)
+#     print(sum1)
+# get_sum(1,2,3,4,0,9,8)
+
+
+# # in the previous example we see how we can handle more than one parameter. Now here the problem is, with this code we can't give key-value pairs as arguments.So how can we handle this?let's see
+
+# def handle_parametes_and_dictionary(num1,*args,**kwargs):
+#     print(sum((num1,)+args))
+#     print(kwargs)
+# handle_parametes_and_dictionary(0,2,3,4,name="nasir")
+
+
+# def handle_parameter(**kwargs):
+#     for key,value in kwargs.items():
+#         print(key,value)
+
+# handle_parameter(name="nasir",age=20)
