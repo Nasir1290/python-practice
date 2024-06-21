135e34b49e416a6c08bc2fed2a9c92b81de0dafa
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-21 08:28:00 +0600

    Practice recursion in python

--- function_in_python.py
+++ function_in_python.py
@@ -36,9 +36,43 @@
 # handle_parameter(name="nasir",age=20)
 
 
-
 # lambda function in python
-# it's a one line function which is work like that
-add_ten = lambda x:x+10
-print(add_ten(50))
 
+# # it's a one line function which is work like that
+# add_ten = lambda x:x+10
+# print(add_ten(50))
+
+
+# lambda function with higher order function like map(),filter(),sorted(),reduce()
+
+# # lambda with map function
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# squared_numbers = (map(lambda x: x**2, numbers))
+# # convert it into a list because list return a reference value
+# print(list(squared_numbers))
+
+
+# # lambda with filter function
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# filtered_numbers = list(filter(lambda x:x%2!=0,numbers))
+# #  convert it into a list because filter return a reference value
+# print(filtered_numbers)
+
+
+# # lambda function in sorted function
+
+# # fruite_list=[("apple",1),("banana",2),("cucumber",3),("mango",4),("Jackfruit",5)]
+# # dic=[{key:value} for key,value in fruite_list]
+# # print(dic)
+
+# fruites=[{'apple': 1}, {'banana': 2}, {'cucumber': 3}, {'mango': 4}, {'Jackfruit': 5}]
+# sorted_fruites=sorted(fruites,key=lambda x:list(x.values())[0])
+# print(sorted_fruites)
+
+
+# # reducing boilerplate code by using lambda function
+
+# def apply_function(x, func):
+#     print(func(x))
+# apply_function(5,lambda x:x+10)
