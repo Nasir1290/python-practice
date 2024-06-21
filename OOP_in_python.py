891eee4ccdcba7d3366063bf5a8657dee5bba84c
Nasir1290 <nasirmia12901290@gmail.com>
2024-06-21 19:39:06 +0600

    practice OOP in python

---
+++ OOP_in_python.py
@@ -0,0 +1,3 @@
+# OOP = object oriented programming is one of the most important topic in python👇👇👇
+# Class in oop= Class means blueprint of any object in oop. where the functionality of the object will discribed or you can say describe it's functionality.
+
